e8c5fd189ec97c9b614c38fdf6bd2968ba1b9648
Dessana Santos <dessanacms@gmail.com>
2021-11-10 16:26:32 +0000

    First Commit

---
+++ Tipsy/CalculatorViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPcButton: UIButton!
+    @IBOutlet weak var tenPcButton: UIButton!
+    @IBOutlet weak var twentyPcButton: UIButton!
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
