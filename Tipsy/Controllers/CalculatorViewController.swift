5dbcf3b91891ef5898528e75b33b09f1aaa87afa
Dessana Santos <dessanacms@gmail.com>
2021-11-10 19:00:13 +0000

    Tip to Apply

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -15,7 +15,13 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     
+    
+    // Tracker to print the value when the calculate button got pressed.
+    //// it's mandatory to select one of the tip options, in order to 'substitute' the value
+    var tip = 0.0
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+        
         
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
@@ -23,15 +29,28 @@ class CalculatorViewController: UIViewController {
                 
         //Make the button that triggered the IBAction selected.
         sender.isSelected = true
+        
+        // Getting button name and removes the last character.
+        var buttonName = sender.currentTitle!
+        buttonName.removeLast()
+        
+        
+        // How much tip to apply
+        /// Transforming buttonName into a Double and divides by 100 to show decimal places results.
+        let buttonNameAsADouble = Double(buttonName)!
+        tip = buttonNameAsADouble / 100
             
         
     }
+    
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        print(tip)
     }
 
 
